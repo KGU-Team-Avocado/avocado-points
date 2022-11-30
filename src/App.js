8d72900f1dc6034d3d7a31576ae4a93f7839f880
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-11-30 16:37:49 +0900

    feat: user 객체 생성 및 총 장학금 총합 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Card, Container, Stack, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function App() {
+
+  const scholarship = Object.values({
+    default: 500000,
+    members: 100000 * 6,
+    rewards: 300000,
+    funding: 116000
+  }).reduce((a, b) => a + b, 0);
+
+  const [users, setUsers] = useState(
+    ["gabrielyoon7", "wlstn", "201912069",
+      "hido", "seeun", "yeonsu"].map((id) => ({
+        id: id,
+        attendance: 0,
+        login: 0,
+        event: 0
+      }))
+  );
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box>
+      <Container>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          mt={5}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography variant='h3'>아보카도 팀</Typography>
+          <Typography variant='h3'>포인트 계산기</Typography>
+        </Stack>
+        <Stack>
+          <Typography variant='h5'>총 장학금 : {scholarship}원</Typography>
+          <Typography variant='h5'>아이디 : {users.map((user) => user.id).join(', ')}</Typography>
+          <Card variant="outlined">{JSON.stringify(users)}</Card>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Accordion 1</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
 
