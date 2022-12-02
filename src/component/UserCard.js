25bd524bd62beacaf75031497799156e924b1eac
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-12-02 12:26:25 +0900

    feat: Card 디자인 개선

--- src/component/UserCard.js
+++ src/component/UserCard.js
@@ -1,32 +1,45 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Accordion, AccordionDetails, AccordionSummary, Box, Card, Divider, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Card, Divider, Stack, Typography } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function UserCard({ user }) {
+    const MiniCard = ({ children }) => {
+        return (
+            <Card>
+                <Stack px={2} py={1}>
+                    {children}
+                </Stack>
+            </Card>
+        )
+    }
     return (
         <>
             <Card variant="outlined">
-                <Box p={3}>
-                    <Typography variant='h5'>{user.id}</Typography>
-                    <Divider/>
-                    <Typography>출석(5점) {user.attendance}회 : {5*user.attendance}점</Typography>
+                <Stack p={3} spacing={1}>
+                    <Typography variant='h5' sx={{ fontWeight: 'bold' }}>{user.id}</Typography>
+                    <Divider />
+                    <MiniCard>
+                        <Typography>출석(5점) {user.attendance}회 : {5 * user.attendance}점</Typography>
+                    </MiniCard>
                     <Accordion>
                         <AccordionSummary
                             expandIcon={<ExpandMoreIcon />}
                             aria-controls="panel1a-content"
                             id="panel1a-header"
                         >
-                            <Typography>로그인(2점) {user.login.length}회 : {2*user.login.length}점</Typography>
+                            <Typography>로그인(2점) {user.login.length}회 : {2 * user.login.length}점</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
-                            {user.login.map((el)=><Typography key={el}>{el}</Typography>)}
+                            {user.login.map((el) => <Typography key={el}>{el}</Typography>)}
                         </AccordionDetails>
                     </Accordion>
-                    <Typography>이벤트 : {user.event}점</Typography>
-                    <Divider/>
+                    <MiniCard>
+                        <Typography>이벤트 : {user.event}점</Typography>
+                    </MiniCard>
+                    <Divider />
                     <Typography variant='h6'>총점 : {user.total}점</Typography>
-                </Box>
+                </Stack>
             </Card>
         </>
     );
